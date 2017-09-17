f6c092a773674f56c143d635146e9b6fd8b1127d
Ashray <ashray14aggarwal@gmail.com>
2017-09-17 07:21:42 +0530

    accounts app complete

--- starsocial/settings.py
+++ starsocial/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3'
+    'bootstrap3',
     'accounts',
 ]
 
@@ -122,3 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
+
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
